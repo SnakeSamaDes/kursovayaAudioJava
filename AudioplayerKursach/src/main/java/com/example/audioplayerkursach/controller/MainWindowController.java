f2ba12532e2d2048e4561051ffe3973fbedf0efc
SnakeSamaDes <116975420+SnakeSamaDes@users.noreply.github.com>
2023-06-09 02:14:13 +0900

    add visualiser, initiall imp

--- AudioplayerKursach/src/main/java/com/example/audioplayerkursach/controller/MainWindowController.java
+++ AudioplayerKursach/src/main/java/com/example/audioplayerkursach/controller/MainWindowController.java
@@ -25,12 +25,14 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.ResourceBundle;
 
-public class MainWindowController{
-
+public class MainWindowController implements Initializable {
+    private final int BANDS = 128;
+    private final double INTERVAL = 0.01;
+    private final double DROP = 0.35;
 
     private MediaPlayer mediaplayer;
     private double volume;
-
+    private XYChart.Data[] chartData;
 
     @FXML
     private Slider timeSlider;
@@ -41,28 +43,78 @@ public class MainWindowController{
     @FXML
     private Label labelArtist;
     @FXML
-    private AreaChart<String, Number> spektrum;
+    private AreaChart<String, Number> spektr;
     @FXML
     private void openfile(ActionEvent actionEvent) {
         try {
             FileChooser fileChooser = new FileChooser();
             fileChooser.getExtensionFilters()
-                    .add(new FileChooser.ExtensionFilter("MP3, WAV", "*.mp3", "*.wav"));
+                    .add(new FileChooser.ExtensionFilter("MP3, WAV", "*.mp3","*.wav"));
             try {
                 File file = fileChooser.showOpenDialog(null);
                 Media media = new Media(file.toURI().toString());
-                if (mediaplayer != null) {
+                if(mediaplayer!=null){
                     mediaplayer.dispose();
                 }
                 mediaplayer = new MediaPlayer(media);
-            } catch (NullPointerException nullPointerException) {
+            }catch (NullPointerException nullPointerException){
                 nullPointerException.printStackTrace();
-            }}}
+            }
+            // timeSlider
+            mediaplayer.setOnReady(()->{
+                try {
+                    String artist = (String) mediaplayer.getMedia().getMetadata().get("artist");
+                    String title = (String) mediaplayer.getMedia().getMetadata().get("title");
+                    labelArtist.setText(artist != null ? artist : "");
+                    labelTitle.setText(title != null ? title : "");
+                    timeSlider.setMin(0);
+                    timeSlider.setMax(mediaplayer.getMedia().getDuration().toSeconds());
+                    timeSlider.setValue(0);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            });
+            mediaplayer.setAudioSpectrumListener(new SpektrumListener());
+            //lister
+            mediaplayer.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+                @Override
+                public void changed(ObservableValue<? extends Duration> observableValue, Duration duration, Duration t1) {
+                    Duration d = mediaplayer.getCurrentTime();
+                    timeSlider.setValue(d.toSeconds());
+                }
+            });
+            timeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+                @Override
+                public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                    if(timeSlider.isPressed()){
+                        double value = timeSlider.getValue();
+                        mediaplayer.seek(new Duration(value*1000));
+                    }
+                }
+            });
+            //volumeSlider change volume
+            volumeSlider.valueProperty().addListener(new InvalidationListener() {
+                @Override
+                public void invalidated(Observable observable) {
+                    mediaplayer.setVolume(volumeSlider.getValue());
+                }
+            });
+            if (volumeSlider.isPressed()) {
+                volume = volumeSlider.getValue();
+                if (mediaplayer != null) {
+                    mediaplayer.setVolume(volume);
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
     @FXML
     private void playClick(ActionEvent actionEvent) {
         try {
             mediaplayer.play();
-
+            mediaplayer.setAudioSpectrumInterval(INTERVAL);
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -95,6 +147,39 @@ public class MainWindowController{
         }
     }
 
-
-    
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        volume = 1.0;
+        XYChart.Series<String, Number> series1 = new XYChart.Series<>();
+        chartData = new XYChart.Data[BANDS+2];
+        for (int i = 0; i < chartData.length; i++) {
+            chartData[i] = new XYChart.Data<>(Integer.toString(i + 1), 0);
+            series1.getData().add(chartData[i]);
+        }
+        spektr.getData().add(series1);
+    }
+    private float[] createFilledBuffer(int size, float fillValue) {
+        float[] floats = new float[size];
+        Arrays.fill(floats, fillValue);
+        return floats;
+    }
+    private class SpektrumListener implements AudioSpectrumListener {
+        float[] buffer = createFilledBuffer(BANDS, mediaplayer.getAudioSpectrumThreshold());
+        @Override
+        public void spectrumDataUpdate(double timestamp, double duration, float[] magnitudes, float[] phases) {
+            Platform.runLater(() -> {
+                chartData[0].setYValue(0);
+                chartData[BANDS].setYValue(0);
+                for (int i = 0; i < magnitudes.length; i++) {
+                    if (magnitudes[i] >= buffer[i]) {
+                        buffer[i] = magnitudes[i];
+                        chartData[i + 1].setYValue(magnitudes[i] - mediaplayer.getAudioSpectrumThreshold());
+                    } else {
+                        chartData[i + 1].setYValue(buffer[i] - mediaplayer.getAudioSpectrumThreshold());
+                        buffer[i] -= DROP;
+                    }
+                }
+            });
+        }
+    }
 }
