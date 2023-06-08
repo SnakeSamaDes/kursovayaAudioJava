ed83ed3c0fca6c31954b77ff073623a0e328ca8c
SnakeSamaDes <116975420+SnakeSamaDes@users.noreply.github.com>
2023-06-09 02:03:12 +0900

    connected controller

--- AudioplayerKursach/src/main/java/com/example/audioplayerkursach/Main.java
+++ AudioplayerKursach/src/main/java/com/example/audioplayerkursach/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainWindow.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view/MainWindow.fxml"));
             Scene scene = new Scene(fxmlLoader.load(), 700, 650);
             stage.setTitle("Audioplayer");
             stage.setScene(scene);
--- AudioplayerKursach/src/main/java/com/example/audioplayerkursach/controller/MainWindowController.java
+++ AudioplayerKursach/src/main/java/com/example/audioplayerkursach/controller/MainWindowController.java
@@ -1,8 +1,59 @@
 package com.example.audioplayerkursach.controller;
 
+import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.AudioSpectrumListener;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.FileChooser;
+import javafx.util.Duration;
 
-public class MainWindowController {
+import java.io.File;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
+public class MainWindowController{
+    @FXML
+    private Slider timeSlider;
+    @FXML
+    private Slider volumeSlider;
+    @FXML
+    private Label labelTitle;
+    @FXML
+    private Label labelArtist;
+    @FXML
+    private AreaChart<String, Number> spektrum;
+    @FXML
+    private void openfile(ActionEvent actionEvent) {
+    }
+    @FXML
+    private void playClick(ActionEvent actionEvent) {
+    }
+    @FXML
+    private void stopClick(ActionEvent actionEvent) {
+    }
+    @FXML
+    private void pauseClick(ActionEvent actionEvent) {
+    }
+    @FXML
+    private void closeApplication(ActionEvent actionEvent) {
+    }
+    @FXML
+    private void refreshClick(ActionEvent actionEvent) {
+    }
+
 
 }
