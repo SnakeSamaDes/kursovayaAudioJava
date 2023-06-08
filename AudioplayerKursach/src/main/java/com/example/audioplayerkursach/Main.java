18029b2bd9797e4e2fabc4bb54a2119fb8bbfbf4
SnakeSamaDes <116975420+SnakeSamaDes@users.noreply.github.com>
2023-06-09 01:51:46 +0900

    main.java final

--- AudioplayerKursach/src/main/java/com/example/audioplayerkursach/Main.java
+++ AudioplayerKursach/src/main/java/com/example/audioplayerkursach/Main.java
@@ -10,11 +10,15 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainWindow.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 700, 650);
+            stage.setTitle("Audioplayer");
+            stage.setScene(scene);
+            stage.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
